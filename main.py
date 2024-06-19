62e2fa79b3a76df360fc7143c062fc2fd8ca230b
Baltemor369 <thomespo@gmail.com>
2024-06-19 16:48:29 +0200

    add run.bat

--- main.py
+++ main.py
@@ -4,8 +4,3 @@ if __name__=="__main__":
     win = Ui()
     win.Game_tab()
     win.mainloop()
-
-# TODO :
-# + Add menu : create own color pallet, number of attempts
-# + Add give up button
-# + more esthetic
